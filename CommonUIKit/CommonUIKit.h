dff2bbe585da70c22260db5273656a1e4cabde3b
侯逸仙 <houyixian@sunlands.com>
2019-10-29 14:16:43 +0800

    增加CommonUIKit module

---
+++ CommonUIKit/CommonUIKit.h
@@ -0,0 +1,19 @@
+//
+//  CommonUIKit.h
+//  CommonUIKit
+//
+//  Created by 侯逸仙 on 2019/10/29.
+//  Copyright © 2019 侯逸仙. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommonUIKit.
+FOUNDATION_EXPORT double CommonUIKitVersionNumber;
+
+//! Project version string for CommonUIKit.
+FOUNDATION_EXPORT const unsigned char CommonUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonUIKit/PublicHeader.h>
+
+
